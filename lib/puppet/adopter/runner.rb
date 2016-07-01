7890ccaceb01e0aee54c5023d29a40e4373b7b2c
Thomas Linkin <trlinkin@gmail.com>
2016-07-01 16:27:50 -0400

    Add "Runner" class to execute agents via PCP
    This commit adds a new class that will execute Puppet Agent runs on the
    test population using PCP. It will also bring back the results if
    delivered in time and use the transaction data to better pinpoint the
    events used for variation processing.

---
+++ lib/puppet/adopter/runner.rb
@@ -0,0 +1,100 @@
+require 'pcp-client'
+require 'timeout'
+require 'ruby-progressbar'
+
+class Puppet::Adopter::Runner
+
+  def initialize(group, config={})
+    @group = group
+    @config = config
+  end
+
+  def config=(config)
+    @config = config
+  end
+
+  def config
+    {
+      :server      => "wss://#{Puppet['server']}:8142/pcp/v1.0",
+      :ssl_key     => Puppet['hostprivkey'],
+      :ssl_cert    => Puppet['hostcert'],
+      :ssl_ca_cert => Puppet['localcacert'],
+    }.merge @config
+  end
+
+  def client
+    if not EM.reactor_running?
+      Thread.new { EM.run }
+      Thread.pass until EM.reactor_running?
+    end
+
+    @client ||= PCP::Client.new(config)
+  end
+
+  def run(timeout = 60)
+
+    if not client.associated?
+      raise "Failure connecting to PCP broker" unless client.connect
+    end
+
+   run_message = prepare_message(@group.certnames)
+   transaction_id = run_message.data['transaction_id']
+   completed_list = Array.new
+
+   client.on_message = proc do |message|
+     Puppet.debug "Received a message over PCP - #{message.data['transaction_id']}"
+     if message.data['transaction_id'] == transaction_id and message.data['results']
+       sender = message[:sender].chomp('/agent').slice(6..-1)
+       @group[sender].use_transaction_uuid( message.data['results']['transaction_uuid'] )
+
+       completed_list << sender
+       #tell progress bar to increment
+     end
+  end
+
+  run_message.expires(10)
+  client.send(message)
+
+  progressbar = ProgressBar.create(:total => @group.certnames.count, :title => "Nodes Complete", :length => 80)
+
+  begin
+    Timeout::timeout(timeout) {
+      until completed_list.count == @group.certnames.count
+        progressbar.progress = completed_list.count
+        sleep 0.5
+      end
+    }
+  rescue Timeout::Error
+    Puppet.debug "Execution expired while waiting for Puppet Agents to complete run"
+  end
+
+  completed_list
+  end
+
+  def prepare_message(node_list)
+
+    targets = node_list.map{|node| "pcp://#{node}/agent" }
+
+    message = PCP::Message.new({
+      :message_type => 'http://puppetlabs.com/rpc_non_blocking_request',
+      :targets      => targets
+    })
+
+    params = {
+      :env => [],
+      :flags => ['--onetime']
+    }
+
+    message_data = {
+      :transaction_id => SecureRandom.uuid,
+      :module         => 'pxp-module-puppet',
+      :action         => 'run',
+      :params         => params,
+      :notify_outcome => true
+    }
+
+    message.data = message_data.to_json
+
+    message
+  end
+end
