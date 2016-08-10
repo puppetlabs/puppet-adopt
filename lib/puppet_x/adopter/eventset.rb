4e3b739427f8ee5d3bcf3cc9abf9c0f410369954
Zee Alexander <zee@puppet.com>
2016-08-09 18:58:28 -0700

    Add validation to eventsets
    Here's an example of where this is better than set; we can make it
    behave just like set but only accept events.
    
    With the previous implementation, you could throw any old garbage into
    the event Set instances, and it wouldn't blow up until later.

--- lib/puppet_x/adopter/eventset.rb
+++ lib/puppet_x/adopter/eventset.rb
@@ -1,5 +1,13 @@
 class PuppetX::Adopter::EventSet < Set
   def initialize(events)
+    self.validate(events)
     super(events)
+  end
+
+  def validate(events)
+    validated_events = events.select { |event| event.instance_of? PuppetX::Adopter::Event }
+    if events.length != validated_events.length
+      raise ArgumentError.new('One of the events passed to EventSet.new is not an instance of PuppetX::Adopter::Event')
+    end
   end
 end
